e4fa57bdfac96504ba18386bbd4c73c69182db43
Baptiste Bentivoglio <baptiste@benti.fr>
2021-05-11 19:09:20 +0200

    add cart offcanvas on catalog

---
+++ src/components/CartOffcanvas.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasCart" aria-labelledby="offcanvasCartLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasCartLabel">Votre Panier</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <div>
+        Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/catalog">Catalog</router-link> |
+        <router-link to="/about">About</router-link> |
+        <button
+            @click="cartOverlay"
+            class="btn btn-primary"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasCart"
+            aria-controls="offcanvasCart"
+        >
+        Voir le Panier
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        cartOverlay(event) {
+            console.log(event)
+            // toggle cart visibility
+        }
+    }
+}
+</script>
+
+<style>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
