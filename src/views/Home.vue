b448cc8e777f8f932ec4cbf3898a46ec78605f50
Tyfannia Nathaniel <ranaivosontyfannia@gmail.com>
2021-06-04 16:13:07 +0200

    Création composant New

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <New collection="Nouvelle collection PE21" msg="Notre sélection pour les beaux jours"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import New from '@/components/New.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld, 
+    New
   }
 }
 </script>
