b51c5bc325073f281cd7d63ecff7839eb1632b12
Tyfannia Nathaniel <ranaivosontyfannia@gmail.com>
2021-06-04 16:22:41 +0200

    Suppression component HelloWorld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <New collection="Nouvelle collection PE21" msg="Notre sélection pour les beaux jours"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 import New from '@/components/New.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld, 
     New
   }
 }
