ab6533547350e497ddbe15cd2405cd11a52118f6
Tyfannia Nathaniel <ranaivosontyfannia@gmail.com>
2021-06-08 11:44:30 +0200

    Création composant SocialMedia

---
+++ src/components/SocialMedia.vue
@@ -0,0 +1,27 @@
+<template>
+    <section id="socialmedia">
+        <ul class="d-inline-flex">
+            <li class="nav-item">
+                <router-link class="nav-link pe-0" to="#">
+                    <svg class="bi" width="21" height="21" fill="currentColor"><use xlink:href="assets/bootstrap-icons.svg#instagram"/></svg>
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link pe-0" to="#">
+                    <svg class="bi" width="21" height="21" fill="currentColor"><use xlink:href="assets/bootstrap-icons.svg#facebook"/></svg>
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-link pe-0" to="#">
+                    <svg class="bi" width="21" height="21" fill="currentColor"><use xlink:href="assets/bootstrap-icons.svg#twitter"/></svg>
+                </router-link>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'SocialMedia'
+}
+</script>
