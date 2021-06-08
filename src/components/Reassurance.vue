9d2522ea49ff0ff25720de6f82d0810ca39795d7
Tyfannia Nathaniel <ranaivosontyfannia@gmail.com>
2021-06-08 09:57:18 +0200

    Création composant Reassurance

---
+++ src/components/Reassurance.vue
@@ -0,0 +1,44 @@
+<template>
+    <section>
+        <ul class="d-flex row text-center justify-content-between">
+            <li class="col-6 col-md-3">
+                <svg class="bi" width="21" height="21" fill="currentColor"><use xlink:href="assets/bootstrap-icons.svg#arrow-counterclockwise"/></svg>
+                <p>Retour gratuit sous <br> 30 jours</p>
+                
+            </li>
+            <li class="col-6 col-md-3">
+                <svg class="bi" width="21" height="21" fill="currentColor"><use xlink:href="assets/bootstrap-icons.svg#truck"/></svg>
+                <p>Livraison à partir de <br> 50€ d'achat</p>
+                
+            </li>
+            <li class="col-6 col-md-3">
+                <svg class="bi" width="21" height="21" fill="currentColor"><use xlink:href="assets/bootstrap-icons.svg#headset"/></svg>
+                <p>Service client dédié</p>
+                
+            </li>
+            <li class="col-6 col-md-3">
+                <svg class="bi" width="21" height="21" fill="currentColor"><use xlink:href="assets/bootstrap-icons.svg#lock-fill"/></svg>
+                <p>Paiement sécurisé</p>
+                
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Reassurance'
+}
+</script>
+
+<style scoped>
+    ul{
+        padding: 0;
+    }
+
+    svg{
+        /* Centrer */
+        display: block;
+        margin: auto;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <New collection="Nouvelle collection PE21" msg="Notre sélection pour les beaux jours"/>
     <Bestsellers/>
+    <Reassurance />
     <Newsletter />
 
   </div>
@@ -12,7 +13,7 @@
 import New from '@/components/New.vue';
 import Bestsellers from '@/components/Bestsellers.vue';
 import Newsletter from '@/components/Newsletter.vue';
-
+import Reassurance from '@/components/Reassurance.vue';
 
 
 export default {
@@ -20,6 +21,7 @@ export default {
   components: {
     New,
     Bestsellers,
+    Reassurance,
     Newsletter
   }
 }
