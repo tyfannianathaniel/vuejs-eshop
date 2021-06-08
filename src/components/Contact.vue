57c708f3f098f45d392f53c6ff5dd37809f6eeac
Tyfannia Nathaniel <ranaivosontyfannia@gmail.com>
2021-06-08 16:28:14 +0200

    Création composant Contact

---
+++ src/components/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="text-center">
+        <p>
+            Nous appeler 
+            <a href="tel:+330100000000">(+33) 01 00 00 00 00</a>
+        </p>
+        <p>
+            Nous écrire 
+            <a href="mailto:contact@myshop.com">contact@myshop.com</a>
+        </p>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'Contact'
+}
+</script>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,6 +3,7 @@
         <Reassurance />
         <Newsletter />
         <SocialMedia />
+        <Contact />
     </footer>
 </template>
 
@@ -11,13 +12,16 @@
 import Reassurance from '@/components/Reassurance.vue';
 import Newsletter from '@/components/Newsletter.vue';
 import SocialMedia from '@/components/SocialMedia.vue';
+import Contact from '@/components/Contact.vue';
+
 
 export default {
     name: 'Footer',
     components: {
         Reassurance,
         Newsletter,
-        SocialMedia
+        SocialMedia,
+        Contact
     }
 }
 </script>
